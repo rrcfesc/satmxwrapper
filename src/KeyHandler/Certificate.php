682c7eb7c28109bc3a74766760c2d94d61ed5a43
Ricardo Ruiz <rrcfesc@gmail.com>
2020-07-18 09:30:30 -0500

    [ADD] test and certificateHandler

--- src/KeyHandler/Certificate.php
+++ src/KeyHandler/Certificate.php
@@ -3,25 +3,85 @@
 declare(strict_types=1);
 
 namespace Rioxygen\SatMx\KeyHandler;
+use RuntimeException;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
 
 final class Certificate
 {
     /** @var string */
-    private $certPath;
+    private $basePath;
 
     /** @var string */
-    private $keyPath;
+    private $rfc;
 
     /** @var string */
-    private $passwd;
+    private $cerFile;
+
+    /** @var string */
+    private $keyFile;
+
+    /** @var string */
+    private $pemFile;
+
+    /** @var string */
+    private $password;
 
     public function __construct(
-        $certPath,
-        $keyPath,
-        $passwd
+        string $basePath,
+        string $rfc,
+        string $password
     ) {
-
+        $this->basePath = $basePath;
+        $this->rfc = $rfc;
+        $this->password = $password;
+        $this->validateFilePath();
+        $this->validateCerFile();
+        $this->validateCertificatePassword();
 
     }
 
+    private function validateCerFile():void
+    {
+        $process = new Process([
+            'openssl',
+            'x509',
+            '-inform',
+            'der',
+            '-in',
+           $this->cerFile,
+            '-out',
+            $this->pemFile,
+        ]);
+        $filesystem = new Filesystem();
+
+        if (!$filesystem->exists($this->pemFile)) {
+            throw new RuntimeException("Invalid cer File");
+        }
+    }
+
+    private function validateCertificatePassword():void
+    {
+
+    }
+
+    private function validateFilePath():void
+    {
+
+        $filesystem = new Filesystem();
+        $cerFile = sprintf("%s/%s.cer", $this->basePath, $this->rfc);
+        $keyFile = sprintf("%s/%s.key", $this->basePath, $this->rfc);
+        if (!$filesystem->exists($cerFile)) {
+            throw new RuntimeException("Missing cer file");
+        }
+        if (!$filesystem->exists($keyFile)) {
+            throw new RuntimeException("Missing key file");
+        }
+
+        $this->cerFile = $cerFile;
+        $this->keyFile = $keyFile;
+        $this->pemFile = $cerFile = sprintf("%s/%s.pem", $this->basePath, $this->rfc);
+    }
 }
---
+++ tests/KeyHandler/CertificateTest.php
@@ -0,0 +1,66 @@
+<?php declare(strict_types=1);
+
+namespace Rioxygen\SatMx\KeyHandler;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Filesystem\Filesystem;
+use RuntimeException;
+
+class CertificateTest extends TestCase
+{
+    /** @var string */
+    private $fixDirectory;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->fixDirectory = sprintf("%s/fixtures", realpath(__DIR__."/../"));
+    }
+
+    public function testCertificate()
+    {
+        $certificateHandler = new Certificate(
+            $this->fixDirectory,
+            'rucr860806s2a',
+            'DKAv9g7fB'
+        );
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @dataProvider getData
+     */
+    public function testExceptions($rfc, $password, $createCer, $createKey, $exceptionMessage)
+    {
+        $filesystem = new Filesystem();
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage($exceptionMessage);
+        $filesystem->remove(sprintf("%s/%s.cer", $this->fixDirectory, $rfc));
+        $filesystem->remove(sprintf("%s/%s.key", $this->fixDirectory, $rfc));
+
+        if ($createCer) {
+            $filesystem->touch(sprintf("%s/%s.cer", $this->fixDirectory, $rfc));
+        }
+        if ($createKey) {
+            $filesystem->touch(sprintf("%s/%s.key", $this->fixDirectory, $rfc));
+        }
+        $certificateHandler = new Certificate(
+            $this->fixDirectory,
+            $rfc,
+            $password
+        );
+
+
+    }
+
+    public function getData(): array
+    {
+        return [
+            ['rucr860806s2a2as', 'asd', false, true, "Missing cer file"],
+            ['rucr860806s2a2as', 'asd', true, false, "Missing key file"],
+            ['rucr860806s2a2as', 'asd', true, true, "Invalid cer File"],
+            ['rucr860806s2a2as', 'asd', false, false, "Missing cer file"],
+        ];
+    }
+
+}
